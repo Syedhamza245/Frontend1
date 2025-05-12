773e7e572db734ca0ecfe355981c5b488390c0f6
syedhamza245 <syed2468hamza@gmail.com>
2025-05-12 12:18:46 +0500

    commit changes to add pricing page

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
      <Hero/>
-     {/* <TemplateSelection/>
-     <Pricing/> */}
+     {/* <TemplateSelection/> */}
+     <Pricing/> 
     </div>
   );
 }
